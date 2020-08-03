0a72d38d2572a6bc7a42a0a588fc61c13dc7c571
thiagodiasv <thiago76ers@gmail.com>
2020-08-03 12:42:20 -0300

    fix: staticfiles

--- pyjobschallenge/settings.py
+++ pyjobschallenge/settings.py
@@ -132,7 +132,7 @@ DEBUG_PROPAGATE_EXCEPTIONS = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = f"{BASE_DIR}/pyjobschallenge/staticfiles"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'pyjobschallenge.storage.WhiteNoiseStaticFilesStorage'
 
 IN_HEROKU = config('IN_HEROKU', default=True)
 
